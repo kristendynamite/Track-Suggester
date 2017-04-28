555a42fa885eea2a9fc856a89067806235742684
Kristen McKee <kristen.dynamite@gmail.com>
2017-04-28 16:32:58 -0700

    add more conditions to alerts to scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,39 @@
-$(function() {
+$(document).ready(function() {
   $("#userInput").submit(function(event) {
-    event.preventDefault();
     var name = $("#name").val();
     var design = $("#design").val();
     var android = $("#android").val();
 
     if (design === "Designing the navigation and layout of a website") && (android === "Yes") {
       $(".courseType").text("Java / Android");
+      $(".java").show();
+      $(".design").hide();
+      $(".cSharp").hide();
 
     } else if (design === "Designing the navigation and layout of a website") && (android === "No") {
       $(".courseType").text("CSS / Design");
+      $(".design").show();
+      $(".java").hide();
+      $(".cSharp").hide();
 
     } else if (design === "Building the infrastructure of an app") && (android === "Yes") {
       $(".courseType").text("Java / Android");
-      // $(".java").show();
-      // $(".design").hide();
-      // $(".cSharp").hide();
+      $(".java").show();
+      $(".design").hide();
+      $(".cSharp").hide();
 
     } else if (design === "Building the infrastructure of an app") && (android === "No") {
       $(".courseType").text("C# / .NET");
-      // $(".cSharp").show();
-      // $(".java").hide();
-      // $(".cSharp").hide();
+      $(".cSharp").show();
+      $(".java").hide();
+      $(".design").hide();
 
     } else if (design === "" || android === "") {
       alert('Selection must be made in dropdown menu');
       $('#suggestion').hide();
     }
 
-    if (name === "" || fiveYears ==="") {
+    if (name === "" || fiveYears === "") {
       alert('Please fill out the form');
       $('#suggestion').hide();
     }
@@ -36,5 +41,7 @@ $(function() {
     $(".nameInput").text(name);
 
     $('#suggestion').show();
+
+    event.preventDefault();
   });
 });
