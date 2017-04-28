fd28312b00f902654090e29f92b80937f3194474
Kristen McKee <kristen.dynamite@gmail.com>
2017-04-28 14:52:12 -0700

    add content to README.md.

---
+++ README.md
@@ -0,0 +1,36 @@
+# _Track Suggester_
+
+#### _Epicodus Track Suggester Webpage, {April 28, 2017}_
+
+#### By _**Kristen McKee**_
+
+## Description
+
+_This application contains a form with a submit button that, when clicked on, shows the user which Epicodus track is right for them. The user must fill out the form to get their suggest track._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open this link your chrome browser: file:///Users/KristenDynamite/Desktop/Track-Suggester/index.html?
+* _Fill out the form
+* _Click on submit to see your results!
+
+## Known Bugs
+
+_There are no known bugs_
+
+## Support and contact details
+
+_If you run into any issues or have any questions about this application, contact kristen.dynamite@gmail.com_
+
+## Technologies Used
+
+_HTML_
+_CSS_
+_Bootstrap_
+_Javascript_
+_jQuery_
+
+### License
+
+Copyright (c) 2016 **_Kristen McKee, Epicodus_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,40 @@
-$(function () {
+$(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
     var name = $("#name").val();
     var design = $("#design").val();
     var android = $("#android").val();
 
-    if ( design === "Building the infrastructure of an app" && android === "Yes") {
-      $(".java").show();
-      $(".design").hide();
-      $(".cSharp").hide();
-    } else if ( design === "Building the infrastructure of an app" && android === "No") {
-      $(".cSharp").show();
-      $(".java").hide();
-      $(".cSharp").hide();
-    } else if ( design === "Designing the navigation and layout of a website") || (design === "Solving for bad website interfaces and poor user experience") && android === "No"{
-    $(".design").show();
-    $(".java").hide();
-    $(".cSharp").hide();
+    if (design === "Designing the navigation and layout of a website") && (android === "Yes") {
+      $(".courseType").text("Java / Android");
+
+    } else if (design === "Designing the navigation and layout of a website") && (android === "No") {
+      $(".courseType").text("CSS / Design");
+
+    } else if (design === "Building the infrastructure of an app") && (android === "Yes") {
+      $(".courseType").text("Java / Android");
+      // $(".java").show();
+      // $(".design").hide();
+      // $(".cSharp").hide();
+
+    } else if (design === "Building the infrastructure of an app") && (android === "No") {
+      $(".courseType").text("C# / .NET");
+      // $(".cSharp").show();
+      // $(".java").hide();
+      // $(".cSharp").hide();
+
+    } else if (design === "" || android === "") {
+      alert('Selection must be made in dropdown menu');
+      $('#suggestion').hide();
     }
 
-    if (name === "") {
-      alert('Fill in your name')
-      $(".suggestion").hide();
+    if (name === "" || fiveYears ==="") {
+      alert('Please fill out the form');
+      $('#suggestion').hide();
     }
 
-    $(".name").text(name);
+    $(".nameInput").text(name);
 
-    $(".suggestion").show();
+    $('#suggestion').show();
   });
 });
